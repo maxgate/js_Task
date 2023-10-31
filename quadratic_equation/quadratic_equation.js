417bdec5893e0008bb91b3a39de7cdf44d309cca
max vincent <kelechivincy@gmail.com>
2023-10-31 02:42:10 -0700

    updated

--- quadratic_equation/quadratic_equation.js
+++ quadratic_equation/quadratic_equation.js
@@ -1,45 +1,44 @@
+" use strict";
+
 // function findQuadEqn(a, b, c) {
-//   // program to solve quadratic equation
-//   let root1, root2;
+// program to solve quadratic equation
+// let root1, root2;
 
-//   // take input from the user
-//   let a = prompt("Enter the first number: ");
-//   let b = prompt("Enter the second number: ");
-//   let c = prompt("Enter the third number: ");
+// // take input from the user
+// let a = prompt("Enter the first number: ");
+// let b = prompt("Enter the second number: ");
+// let c = prompt("Enter the third number: ");
 
-//   // calculate discriminant
-//   let discriminant = b * b - 4 * a * c;
+// // calculate discriminant
+// let discriminant = b * b - 4 * a * c;
 
-//   // condition for real and different roots
-//   if (discriminant > 0) {
-//     root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-//     root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+// // condition for real and different roots
+// if (discriminant > 0) {
+//   root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+//   root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
 
-//     // result
-//     console.log(`The roots of quadratic equation are ${root1} and ${root2}`);
-//   }
-
-//   // condition for real and equal roots
-//   else if (discriminant == 0) {
-//     root1 = root2 = -b / (2 * a);
-
-//     // result
-//     console.log(`The roots of quadratic equation are ${root1} and ${root2}`);
-//   }
-
-//   // if roots are not real
-//   else {
-//     let realPart = (-b / (2 * a)).toFixed(2);
-//     let imagPart = (Math.sqrt(-discriminant) / (2 * a)).toFixed(2);
-
-//     // result
-//     console.log(
-//       `The roots of quadratic equation are ${realPart} + ${imagPart}i and ${realPart} - ${imagPart}i`
-//     );
-//   }
+//   // result
+//   console.log(`The roots of quadratic equation are ${root1} and ${root2}`);
 // }
 
-// console.log(findQuadEqn(1, 6, 5));
+// // condition for real and equal roots
+// else if (discriminant == 0) {
+//   root1 = root2 = -b / (2 * a);
+
+//   // result
+//   console.log(`The roots of quadratic equation are ${root1} and ${root2}`);
+// }
+
+// // if roots are not real
+// else {
+//   let realPart = (-b / (2 * a)).toFixed(2);
+//   let imagPart = (Math.sqrt(-discriminant) / (2 * a)).toFixed(2);
+
+//   // result
+//   console.log(
+//     `The roots of quadratic equation are ${realPart} + ${imagPart}i and ${realPart} - ${imagPart}i`
+//   );
+// }
 
 //////////////////////////////////////////////////
 // JavaScript program to find roots
@@ -56,35 +55,33 @@ function findRoots(a, b, c) {
     return;
   }
 
-  let d = b * b - 4 * a * c;
-  let sqrt_val = Math.sqrt(Math.abs(d));
+  let discriminant = b * b - 4 * a * c;
+  let sqrt_val = Math.sqrt(Math.abs(discriminant));
 
-  if (d > 0) {
+  if (discriminant > 0) {
     console.log("Roots are real and different");
 
-    console.log(
-      (-b + sqrt_val) / (2 * a) + " and " + (-b - sqrt_val) / (2 * a)
-    );
-  } else if (d == 0) {
+    return (-b + sqrt_val) / (2 * a) + " and " + (-b - sqrt_val) / (2 * a);
+  } else if (discriminant == 0) {
     console.log("Roots are real and same");
 
-    console.log(-b / (2 * a) + " and " + -b / (2 * a));
+    return -b / (2 * a) + " and " + -b / (2 * a);
   }
 
   // d < 0
   else {
     console.log("Roots are complex");
 
-    console.log(
-      -b / (2 * a) +
+    return (
+      (-b / (2 * a) +
         " + i" +
         sqrt_val / (2 * a) +
         " and " +
         -b / (2 * a) +
         " - i" +
-        sqrt_val
-    ) /
-      (2 * a);
+        sqrt_val) /
+      (2 * a)
+    );
   }
 }
 
@@ -92,5 +89,5 @@ let a = 1,
   b = -7,
   c = 12;
 
-findRoots(a, b, c);
-// console.log(findRoots());
+// findRoots(a, b, c);
+console.log(findRoots(a, b, c));
