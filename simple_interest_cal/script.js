0118a7805b00aae03c0b7c1ff2cb03b8af6687ad
max vincent <kelechivincy@gmail.com>
2023-11-16 06:28:05 -0800

    updated

--- simple_interest_cal/script.js
+++ simple_interest_cal/script.js
@@ -4,12 +4,27 @@ const principalInput = document.querySelector(".principal_input");
 const calculate = document.querySelector(".cal");
 const interestInput = document.querySelector(".interest_input");
 const termInput = document.querySelector(".term_input");
-// document.querySelector("");
+const display = document.querySelector(".screen");
 const clear = document.querySelector(".clear");
 
-clear.addEventListener("click", function () {
+const simpleInt = function () {
+  // let p, i, t;
+  // principalInput.value = p;
+  // interestInput.value = i;
+  // termInput.value = t;
+  return Number(
+    (principalInput.value * interestInput.value * termInput.value) / 100
+  );
+};
+
+const clearScreen = clear.addEventListener("click", function () {
   console.log("cleared");
   principalInput.value = "";
   interestInput.value = "";
   termInput.value = "";
+  display.value = "screen";
+});
+
+calculate.addEventListener("click", (e) => {
+  display.value = Number(simpleInt());
 });
