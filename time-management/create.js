1e5b1aaf699fea6159d28fc6754105cfc15859d4
max vincent <kelechivincy@gmail.com>
2023-11-27 06:19:27 -0800

    script

---
+++ time-management/create.js
@@ -0,0 +1,37 @@
+const createSchedule = (item) => {
+  const displaySchedule = document.querySelector(".display");
+  const html = `
+  <section>
+    <div class="container justify-content-around small">
+      <div class="col-2 rounded-top shadow text-center text-light pt-1 pb-1" style="background-color:   #6664c2">
+        ${item.time_manager_title}
+      </div>
+    </div>
+    <!--  -->
+    <div class=" container text-center small fst-italic justify-content-around">
+        <div class="col-2 bg-light rounded-bottom shadow small pt-3 pb-2">
+          ${item.time_manager_message}
+        </div>
+    </div>
+</section>`;
+
+  displaySchedule.insertAdjacentHTML("afterend", html);
+};
+
+function createTodosStoragse() {
+  const item = localStorage.getItem("todos");
+  if (!item) {
+    localStorage.setItem("todos", JSON.stringify([]));
+  }
+}
+
+function createNewSchedule() {
+  createTodosStoragse();
+
+  const todos = localStorage.getItem("todos");
+  JSON.parse(todos).forEach((item) => {
+    createSchedule(item);
+  });
+}
+
+createNewSchedule();
--- time-management/script.js
+++ time-management/script.js
@@ -1,87 +1,30 @@
+
 const schedule = document.getElementById("mov");
-const title = document.getElementById("exampleFormControlInput1");
-const note = document.querySelector(".note");
-const displaySchedule = document.querySelector(".display");
-// console.log(note, title);
 
-// const displaySchedule = document.querySelector(".display");
-// const fetchData = localStorage.getItem("time_manager");
-// let previous_data = JSON.parse(fetchData);
-
-// if (fetchData) {
-//   //   console.log(previous_data);
-//   previous_data.map((data, index) => {
-//     document.querySelector(".container-fluid").innerHTML += `
-//             <div class="container-fluid row d-flex justify-content-around small">
-//             <div
-//           class="col-2 rounded-top shadow text-center text-light pt-1 pb-1"
-//           style="background-color: #6664c2"
-//         >
-//           ${data.title}
-//         </div>
-//         </div>
-//         <div class="container-fluid row d-flex justify-content-around small">
-//         <div
-//           class="col-2 rounded-top shadow text-center text-light pt-1 pb-1"
-//           style="background-color: #6664c2"
-//         >
-//           ${data.note}
-//         </div>
-//         </div>
-//             `;
-//   });
-// }
-
-//
-//   console.log(title.value);
-
-//   const dataArry = [];
-//   const data = {
-//     title: title.value,
-//     note: note.value,
-//   };
-
-//   dataArry.push(data);
-
-//   if (!localStorage.getItem("time_manager")) {
-//     localStorage.setItem(
-//       "time_manager",
-//       JSON.stringify([{ title: "title", note: "note" }])
-//     );
-//   }
-//   previous_data.push(data);
-//   console.log(previous_data);
-//   localStorage.setItem("time_manager", JSON.stringify(previous_data));
-
-const titleMe = title.value;
-const message = note.value;
-
-const createSchedule = () => {
-  const html = `<section>
-            <div class="container-fluid row d-flex justify-content-around small">
-              <div
-                class="col-2 rounded-top shadow text-center text-light pt-1 pb-1"
-                style="background-color: #6664c2"
-              >
-                ${titleMe}
-              </div>
-              <!--  -->
-            <div
-              class="container-fluid row d-flex text-center small fst-italic justify-content-around"
-            >
-              <div class="col-2 bg-light rounded-bottom shadow small pt-3 pb-2">
-                ${message}
-              </div>`;
-
-  schedule.insertAdjacentHTML("afterend", html);
-  //   console.log("createSchedule");
-};
+function createTodosStoragse() {
+  const item = localStorage.getItem("todos");
+  if (!item) {
+    localStorage.setItem("todos", JSON.stringify([]));
+  }
+}
 
 schedule.addEventListener("click", function () {
-  window.location.href = "./index.html";
-  createSchedule();
+  createTodosStoragse();
 
-  titleMe = displaySchedule.value;
-  message = displaySchedule;
-  displaySchedule = this.innerHTML;
+  // Save titleMe and message in localStorage
+  const todos = localStorage.getItem("todos");
+  const todoArray = JSON.parse(todos);
+  const title = document.getElementById("exampleFormControlInput1").value;
+  const note = document.querySelector(".note").value;
+  const newTodoArray = [
+    ...todoArray,
+    { time_manager_message: note, time_manager_title: title },
+  ];
+  localStorage.setItem("todos", JSON.stringify(newTodoArray));
+  // localStorage.setItem("time_manager_message", JSON.stringify(message));
+  // localStorage.setItem("time_manager_title", JSON.stringify(titleMe));
+
+  // Retrieve from localStorage
+
+  window.location.href = "./index.html";
 });
